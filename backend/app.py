d01971ca50725479a679c2aa8a26fb97fc3bafe5
Dimianich <58258385+Dimianich@users.noreply.github.com>
2021-09-16 01:35:19 +0300

    init

---
+++ backend/app.py
@@ -0,0 +1,54 @@
+﻿from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from flaskext.mysql import MySQL
+from redis import Redis
+
+from resources.data import (
+    DataDB,
+    DataRedis,
+    DataLog
+)
+from resources.user import (
+    UserRegistration,
+    UserLogin
+)
+
+import init_db
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.cfg', silent=True)
+
+redis = Redis(host='redis', port=6379)
+jwt = JWTManager(app)
+api = Api(app)
+CORS(app)
+mysql = MySQL()
+mysql.init_app(app)
+
+
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):
+    if (identity == "admin"):
+        return {"is_admin": True}
+    return {"is_admin": False}
+
+
+api.add_resource(UserRegistration, '/registration')
+api.add_resource(UserLogin, '/login')
+api.add_resource(DataDB, '/db')
+api.add_resource(DataRedis, '/redis')
+api.add_resource(DataLog, '/log')
+
+
+@app.before_first_request
+def create_table():
+    try:
+        init_db.create_tables()
+    except:
+        app.logger.error("Ошибка при создании таблиц в бд")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ backend/init_db.py
@@ -0,0 +1,12 @@
+from app import mysql
+
+
+def create_tables():
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    query = "CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password TEXT(1000) NOT NULL)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS data (info TEXT(1000))"
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
