9904de78aaa9363a47ebc8afba84646d6a657308
Dimianich <58258385+Dimianich@users.noreply.github.com>
2021-09-17 02:28:59 +0300

    Add files via upload

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,111 @@
+﻿<template>
+  <div>
+    <navbar />
+    <div class="buttons">
+      <b-button type="is-primary" outlined @click="getDataDb"
+        >Данные из БД</b-button
+      >
+      <b-button type="is-primary" outlined @click="getDataRedis"
+        >Данные из Redis</b-button
+      >
+      <b-button type="is-primary" outlined @click="getDataLog"
+        >Данные из log.json</b-button
+      >
+    </div>
+    <b-table :data="data" :columns="columns"></b-table>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    navbar: Navbar,
+  },
+  data() {
+    return {
+      data: [],
+      columns: [
+        {
+          field: "info",
+          label: "Info",
+        },
+      ],
+    };
+  },
+  methods: {
+    async getDataDb() {
+      try {
+        let response = await fetch("http://backend:5000/db", {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("user"),
+          },
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          this.data = result;
+        } else {
+          this.$buefy.notification.open({
+            message: "Ошибка HTTP: " + response.status + " " + response.message,
+            position: "is-bottom-right",
+            type: "is-danger",
+          });
+        }
+      } catch (err) {
+        console.log(err.response);
+      }
+    },
+    async getDataRedis() {
+      try {
+        let response = await fetch("http://backend:5000/redis", {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("user"),
+          },
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          this.data = result;
+        } else {
+          this.$buefy.notification.open({
+            message: "Ошибка HTTP: " + response.status + " " + response.message,
+            position: "is-bottom-right",
+            type: "is-danger",
+          });
+        }
+      } catch (err) {
+        console.log(err.response);
+      }
+    },
+    async getDataLog() {
+      try {
+        let response = await fetch("http://backend:5000/log", {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("user"),
+          },
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          this.data = result;
+        } else {
+          this.$buefy.notification.open({
+            message: "Ошибка HTTP: " + response.status + " " + response.message,
+            position: "is-bottom-right",
+            type: "is-danger",
+          });
+        }
+      } catch (err) {
+        console.log(err.response);
+      }
+    },
+  },
+};
+</script>
+
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,78 @@
+﻿<template>
+  <section>
+    <div class="title">Авторизация</div>
+    <b-field label="Имя" horizontal>
+      <b-input type="text" required v-model="login.username"></b-input>
+    </b-field>
+
+    <b-field label="Пароль" horizontal>
+      <b-input type="password" required v-model="login.password"></b-input>
+    </b-field>
+
+    <b-button type="is-primary" @click="logIn">Войти</b-button>
+
+    <div>
+      У вас еще нет аккаунта? Нажмите
+      <router-link to="/register"> сюда </router-link> для регистрации
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      login: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async logIn() {
+      if (this.login.username === "" || this.login.password === "") {
+        this.$buefy.notification.open({
+          message: "Не заполнено поле Имя или Пароль",
+          position: "is-bottom-right",
+          type: "is-danger",
+        });
+        return false;
+      }
+      try {
+        let response = await fetch("http://backend:5000/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(this.login),
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          let token = result.acces_token;
+          localStorage.setItem("user", token);
+          localStorage.setItem("username", this.login.username);
+          this.$router.push("/home");
+        } else {
+          this.$buefy.notification.open({
+            message: "Ошибка HTTP: " + response.status + " " + response.message,
+            position: "is-bottom-right",
+            type: "is-danger",
+          });
+        }
+      } catch (err) {
+        console.log(err.response);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+section {
+  width: 40%;
+  margin: auto;
+  text-align: center;
+}
+</style>
